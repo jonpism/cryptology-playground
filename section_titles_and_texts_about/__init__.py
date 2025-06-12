3b9be91fbe794dc7b6472ef5100dac87682960f1
John Pismisakis <ioanpism@csd.auth.gr>
2025-06-12 21:02:58 +0300

    Add files via upload

---
+++ section_titles_and_texts_about/__init__.py
@@ -0,0 +1,16 @@
+from .titles_and_texts import asymmetric_title, asymmetric_text
+from .titles_and_texts import symmetric_title, symmetric_text
+from .titles_and_texts import ciphers_title, ciphers_text
+from .titles_and_texts import hashalgo_title, hashalgo_text
+from .titles_and_texts import encoders_title, encoders_text
+from .titles_and_texts import converters_title, converters_text
+from .titles_and_texts import file_handling_title, file_handling_text
+from .titles_and_texts import othertools_title, othertools_text
+from .titles_and_texts import cryptanalysis_title, cryptanalysis_text
+
+
+__all__ = ["asymmetric_title", "asymmetric_text", "symmetric_title", "symmetric_text",
+           "ciphers_title", "ciphers_text", "hashalgo_title", "hashalgo_text",
+           "encoders_title", "encoders_text", "converters_title", "converters_text",
+           "file_handling_title", "file_handling_text", "othertools_title", "othertools_text",
+           "cryptanalysis_title", "cryptanalysis_text"]
